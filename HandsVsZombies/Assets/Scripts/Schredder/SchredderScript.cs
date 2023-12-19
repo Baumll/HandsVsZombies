7ffd3cefaaf78aff09b663c3c9de918376b105b0
Baumll <adriangame@gmx.de>
2023-12-19 19:53:54 +0100

    final

--- HandsVsZombies/Assets/Scripts/Schredder/SchredderScript.cs
+++ HandsVsZombies/Assets/Scripts/Schredder/SchredderScript.cs
@@ -26,7 +26,7 @@ public class SchredderScript : MonoBehaviour
     {
         if (isSchreddering)
         {
-            addBloodLevel(.5f*Time.deltaTime);
+            addBloodLevel(2f*Time.deltaTime);
             deltaSchredderTime += Time.deltaTime;
             if (deltaSchredderTime >= schredderTime)
             {
@@ -36,7 +36,7 @@ public class SchredderScript : MonoBehaviour
         }
         else
         {
-            addBloodLevel(-.05f*Time.deltaTime);
+            addBloodLevel(-.2f*Time.deltaTime);
         }
     }
 
@@ -61,7 +61,7 @@ public class SchredderScript : MonoBehaviour
     public void setBloodLevel(float level){
         foreach(var bloody in bloodyObjectList)
         {
-            bloody.material.SetFloat("_Blodymess", level);
+            bloody.GetComponent<MeshRenderer>().material.SetFloat("_Blodymess", level);
         }
 
     }
@@ -71,7 +71,7 @@ public class SchredderScript : MonoBehaviour
     {
         foreach (var bloody in bloodyObjectList)
         {
-            bloody.material.SetFloat("_Blodymess", getBloodLevel() + amount);
+            bloody.GetComponent<MeshRenderer>().material.SetFloat("_Blodymess", getBloodLevel() + amount);
         }
         return getBloodLevel() + amount;
     }
@@ -80,7 +80,7 @@ public class SchredderScript : MonoBehaviour
     {
         if (bloodyObjectList.Length > 0)
         {
-            return bloodyObjectList[0].material.GetFloat("_Blodymess");
+            return bloodyObjectList[0].GetComponent<MeshRenderer>().material.GetFloat("_Blodymess");
         }
         return -1;
     }
